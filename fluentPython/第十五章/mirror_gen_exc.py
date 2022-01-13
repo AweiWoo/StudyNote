84cfbab4ec3befaba381d79ba7d724ba103951ba
AweiWoo <724862897@qq.com>
2022-01-13 17:15:46 +0800

    20220113

--- fluentPython/第十五章/mirror_gen_exc.py
+++ fluentPython/第十五章/mirror_gen_exc.py
@@ -13,12 +13,12 @@ def looking_glass():
         original_write(text[::-1])
 
     sys.stdout.write = reverse_write
-    msg = ''
+    msg = ''  #创建一个变量，用于保存可能出现的错误消息
     try:
         yield 'JABBERWOCKY'
     except ZeroDivisionError:
         msg = 'Please DO NOT divide by zero'
     finally:
-        sys.stdout.write = original_write
+        sys.stdout.write = original_write  #撤销对 sys.stdout.write 方法所做的猴子补丁
         if msg:
             print(msg)
