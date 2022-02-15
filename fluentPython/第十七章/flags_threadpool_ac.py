1e39a3ac9710279265dbef8133d66611386f2b0e
AweiWoo <724862897@qq.com>
2022-02-15 09:04:00 +0800

    20220215

--- fluentPython/第十七章/flags_threadpool_ac.py
+++ fluentPython/第十七章/flags_threadpool_ac.py
@@ -14,20 +14,20 @@ def donwload_one(cc):  #下载一个图像的函数
     return cc
 
 def download_many(cc_list):
-    cc_list = cc_list[:5]
-    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+    cc_list = cc_list[:5]   #只下载5个对象
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:  
         to_do = []
         for cc in sorted(cc_list):
             future = executor.submit(donwload_one, cc)
-            to_do.append(future)
+            to_do.append(future)   #存储各个future,传给后面的futures.as_completed
             msg = 'Scheduled for {} : {}'
             print(msg.format(cc, future))
         
         results = []
         for future in futures.as_completed(to_do):
-            res = future.result()
+            res = future.result()   #获取future结果
             msg = '{} result: {!r}'
-            print(msg.format(future, res))
+            print(msg.format(future, res))  #显示结果
             results.append(res)
 
     return len(results)
